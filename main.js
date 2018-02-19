9f126633193dfa6e3c151393dd1e2f68fa5ee036
AmandaAldrich <amlaldrich@gmail.com>
2018-02-19 03:27:16 -0800

    minor changes

--- main.js
+++ main.js
@@ -29,9 +29,9 @@ function createArray(rows) { //creates a 2 dimensional array of required height
 }
 
 function fillRandom() { //fill the grid randomly
-    for (var j = 0; j < gridHeight; j++) { //iterate through rows
-        for (var k = 0; k < gridWidth; k++) { //iterate through columns
-            theGrid[j][k] = Math.floor(Math.random()*6);
+    for (var i = 0; i < gridHeight; i++) { //iterate through rows
+        for (var j = 0; j < gridWidth; j++) { //iterate through columns
+            theGrid[i][j] = Math.floor(Math.random()*6);
         }
     }
 }
@@ -43,37 +43,37 @@ function drawGrid() { //draw the contents of the grid onto a canvas
 	//var blue = Math.floor(Math.random() * 256);
 	var liveCount = 0;
 	ctx.clearRect(0, 0, gridHeight, gridWidth); //this should clear the canvas ahead of each redraw
-	for (var j = 1; j < gridHeight; j++) { //iterate through rows
-		for (var k = 1; k < gridWidth; k++) { //iterate through columns
-			if (theGrid[j][k] === 1) {
-				ctx.fillRect(j, k, 1, 1);
+	for (var i = 1; i < gridHeight; i++) { //iterate through rows
+		for (var j = 1; j < gridWidth; j++) { //iterate through columns
+			if (theGrid[i][j] === 1) {
+				ctx.fillRect(i, j, 1, 1);
 				liveCount++;
 				//ctx.fillStyle = "#f7a576";
 				ctx.fillStyle = "#849324";				
 			}
-			if(theGrid[j][k] === 2){
-				ctx.fillRect(j, k, 1, 1);
+			if(theGrid[i][j] === 2){
+				ctx.fillRect(i, j, 1, 1);
 				liveCount++;
 				//ctx.fillStyle = "#25b28a";
 				ctx.fillStyle = "#FFB30F";
 			}
-			if(theGrid[j][k] === 3){
-				ctx.fillRect(j, k, 1, 1);
+			if(theGrid[i][j] === 3){
+				ctx.fillRect(i, j, 1, 1);
 				liveCount++;
 				//ctx.fillStyle = "#b589f4";
 				ctx.fillStyle = "#01295F";
 			}
-			if(theGrid[j][k] === 4){
-				ctx.fillRect(j, k, 1, 1);
+			if(theGrid[i][j] === 4){
+				ctx.fillRect(i, j, 1, 1);
 				liveCount++;
 				//ctx.fillStyle = "#b589f4";
 				ctx.fillStyle = "#FD151B";
 			}
-			if(theGrid[j][k] === 5){
-				ctx.fillRect(j, k, 1, 1);
+			if(theGrid[i][j] === 5){
+				ctx.fillRect(i, j, 1, 1);
 				liveCount++;
 				//ctx.fillStyle = "#b589f4";
-				ctx.fillStyle = "#BEE9E8";
+				ctx.fillStyle = "#437F97";
 			}
 		}
 	}
@@ -85,61 +85,61 @@ function drawGrid() { //draw the contents of the grid onto a canvas
 
 function updateGrid() { //perform one iteration of grid update
        
-    for (var j = 1; j < gridHeight - 1; j++) { //iterate through rows
-        for (var k = 1; k < gridWidth - 1; k++) { //iterate through columns
+    for (var i = 1; i < gridHeight - 1; i++) { //iterate through rows
+        for (var j = 1; j < gridWidth - 1; j++) { //iterate through columns
             var totalCells = 0;
             //add up the total values for the surrounding cells
-            totalCells += theGrid[j - 1][k - 1]; //top left
-            totalCells += theGrid[j - 1][k]; //top center
-            totalCells += theGrid[j - 1][k + 1]; //top right
+            totalCells += theGrid[i - 1][j - 1]; //top left
+            totalCells += theGrid[i - 1][j]; //top center
+            totalCells += theGrid[i - 1][j + 1]; //top right
 			
-            totalCells += theGrid[j][k - 1]; //middle left
-            totalCells += theGrid[j][k + 1]; //middle right
+            totalCells += theGrid[i][j - 1]; //middle left
+            totalCells += theGrid[i][j + 1]; //middle right
 
-            totalCells += theGrid[j + 1][k - 1]; //bottom left
-            totalCells += theGrid[j + 1][k]; //bottom center
-            totalCells += theGrid[j + 1][k + 1]; //bottom right
+            totalCells += theGrid[i + 1][j - 1]; //bottom left
+            totalCells += theGrid[i + 1][j]; //bottom center
+            totalCells += theGrid[i + 1][j + 1]; //bottom right
             //apply the rules to each cell
             switch (totalCells) {
                 case 2:
-                    mirrorGrid[j][k] = theGrid[j][k];
+                    mirrorGrid[i][j] = theGrid[i][j];
                       
                     break;
                 
 				case 3:
-                    mirrorGrid[j][k] = 1; //live
+                    mirrorGrid[i][j] = 1; //live
                         
                     break;
                       
 				case 6:
-                    mirrorGrid[j][k] = 2; //live
+                    mirrorGrid[i][j] = 2; //live
 					
 					break;
 				
 				case 9:
-                    mirrorGrid[j][k] = 3; //live
+                    mirrorGrid[i][j] = 3; //live
                         
                     break;
 					
 				case 12:
-                    mirrorGrid[j][k] = 4; //live
+                    mirrorGrid[i][j] = 4; //live
                         
                     break;
 					
 				case 15:
-                    mirrorGrid[j][k] = 5; //live
+                    mirrorGrid[i][j] = 5; //live
                         
                     break;
 					
 				default:
-                    mirrorGrid[j][k] = 0; //dead
+                    mirrorGrid[i][j] = 0; //dead
             }
         }
     }
 
     //mirror edges to create wraparound effect
 
-    for (var l = 1; l < gridHeight - 1; l++) { //iterate through rows
+    for (var l = 1; l < gridHeight; l++) { //iterate through rows
         //top and bottom
         mirrorGrid[l][0] = mirrorGrid[l][gridHeight - 3];
         mirrorGrid[l][gridHeight - 2] = mirrorGrid[l][1];
